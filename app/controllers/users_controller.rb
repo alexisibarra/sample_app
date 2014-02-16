88b71d5c0e5ec859e360916d486baa6b6f730989
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2014-02-16 15:17:44 -0430

    The user create action with a save and a redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,9 @@ class UsersController < ApplicationController
 		@user = User.new
   end
   def create
-    @user = User.new(params[user_params])    # Not the final implementation!
+    @user = User.new(user_params)    # Not the final implementation!
     if @user.save
-      # Handle a successful save.
+			redirect_to @user
     else
       render 'new'
     end
