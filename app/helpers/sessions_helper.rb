4fdc380ae82ff2b164f01c268eab7187b6026527
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2014-02-18 10:35:08 -0430

    The signed_in? helper method

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,6 +5,9 @@ module SessionsHelper
     user.update_attribute(:remember_token, User.encrypt(remember_token))
     self.current_user = user
   end
+  def signed_in?
+    !current_user.nil?
+  end
   def current_user=(user)
     @current_user = user
   end
