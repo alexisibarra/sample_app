c1cc473bfc8ec0e9b45fce753010761e71385973
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2014-02-19 09:54:09 -0430

    The user edit action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ class UsersController < ApplicationController
   def new
 		@user = User.new
   end
-  def create
+  
+	def create
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
 			sign_in @user
@@ -16,6 +17,11 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+	
 	private
 
     def user_params
